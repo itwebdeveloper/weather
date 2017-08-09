1d890dec8686a601daff3bf18744cc5edf837039
Andrea Cannuni <andreacannuni@gmail.com>
2017-08-09 22:28:18 +0000

    Set UTC as default timezone

--- config.php
+++ config.php
@@ -2,6 +2,7 @@
 ini_set('display_startup_errors', 1);
 ini_set('display_error', 1);
 error_reporting(E_ALL);
+date_default_timezone_set("UTC"); 
 
 $debug = false; // If true the script prints additional debug lines to video
 $private_key='a258a26fcc8493fc074ecd506b71e636';
