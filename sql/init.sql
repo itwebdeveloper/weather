3ea24b05d6990ffc7e8bf02fcbef420c204fbfc7
Andrea Cannuni <andreacannuni@gmail.com>
2017-08-09 22:22:57 +0000

    Optimize database table field lengths

--- sql/init.sql
+++ sql/init.sql
@@ -3,12 +3,12 @@ USE weather;
 
 CREATE TABLE `results` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `latitude` decimal(11,8) NOT NULL,
-  `longitude` decimal(11,8) NOT NULL,
+  `latitude` decimal(7,4) NOT NULL,
+  `longitude` decimal(7,4) NOT NULL,
   `summary` varchar(50) NOT NULL,
-  `temperature` decimal(3,2) NOT NULL,
-  `units` varchar(5) NOT NULL,
-  `icon` enum('clear-day','clear-night','rain','snow','sleet','wind','fog','cloudy','partly-cloudy-day','or partly-cloudy-night') NOT NULL,
+  `temperature` decimal(5,2) NOT NULL,
+  `units` enum('auto','ca','uk2','us','si') NOT NULL,
+  `icon` enum('clear-day','clear-night','rain','snow','sleet','wind','fog','cloudy','partly-cloudy-day','partly-cloudy-night') NOT NULL,
   `created_on` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
