e524064eb139a5e8130f95643748f1d3874db687
Andrea Cannuni <andreacannuni@gmail.com>
2017-08-09 22:17:03 +0000

    Move configuration in separate file

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+ini_set('display_startup_errors', 1);
+ini_set('display_error', 1);
+error_reporting(E_ALL);
+
+$debug = false; // If true the script prints additional debug lines to video
+$private_key='a258a26fcc8493fc074ecd506b71e636';
+$req_latitude=37.8267;
+$req_longitude=-122.4233;
+// $curl_request_endpoint = "https://api.darksky.net/forecast/$private_key/$req_latitude,$req_longitude";
+$curl_request_endpoint = "http://10.0.0.10/darksky_fake/response.php";
+$log_file = "/var/log/dark_sky_proxy/debug.log";
+$email_addresses = "Andrea Cannuni <andreacannuni@gmail.com>";
+
+$db_host = "10.0.0.10";
+$db_user = "foouser";
+$db_pass = "abc";
+$db_name = "weather";
+$charset = 'utf8';
--- scripts/dark_sky_proxy.php
+++ scripts/dark_sky_proxy.php
@@ -1,22 +1,5 @@
 <?php
-ini_set('display_startup_errors', 1);
-ini_set('display_error', 1);
-error_reporting(E_ALL);
-
-$debug = false; // If true the script prints additional debug lines to video
-$private_key='a258a26fcc8493fc074ecd506b71e636';
-$req_latitude=37.8267;
-$req_longitude=-122.4233;
-// $curl_request_endpoint = "https://api.darksky.net/forecast/$private_key/$req_latitude,$req_longitude";
-$curl_request_endpoint = "http://10.0.0.10/darksky_fake/response.php";
-$log_file = "/var/log/dark_sky_proxy/debug.log";
-$email_addresses = "Andrea Cannuni <andreacannuni@gmail.com>";
-
-$db_host = "10.0.0.10";
-$db_user = "foouser";
-$db_pass = "abc";
-$db_name = "weather";
-$charset = 'utf8';
+require_once(__DIR__ .'/../config.php');
 
 function send_request($curl_request_endpoint) {
     global $debug, $log_file, $email_addresses;
@@ -79,8 +62,10 @@ function send_request($curl_request_endpoint) {
             }
             file_put_contents($log_file, "[". date(DATE_ATOM) ."]". $debug_message, FILE_APPEND);
         } else {
-            $debug_message = "[INFO] Successful response:\n". $curl_response. "\n";
-            file_put_contents($log_file, "[". date(DATE_ATOM) ."]". $debug_message, FILE_APPEND);
+            if($debug) {
+                $debug_message = "[INFO] Successful response:\n". $curl_response. "\n";
+                file_put_contents($log_file, "[". date(DATE_ATOM) ."]". $debug_message, FILE_APPEND);
+            }
 
             // Parse JSON response
             $curl_response_array = json_decode($curl_response);
